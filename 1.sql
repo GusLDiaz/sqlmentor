1f7bc0eb5bc4821b966231bc78967d6c024dfd2a
Gus Liakos <Gliakos@cnm.edu>
2018-04-12 15:25:15 -0600

    relatively final draft cleaned up

--- 1.sql
+++ 1.sql
@@ -1,3 +1,8 @@
+
+
+
+
+
 CREATE TABLE profile (
 	-- this creates the attribute for the primary key
 	-- not null means the attribute is required!
@@ -9,35 +14,31 @@ CREATE TABLE profile (
 	profileDateJoined VARCHAR (128) NOT NULL,
 	profileAge TINYINT,
 	profileGender VARCHAR (32),
-	profileAvail BIT(1) NOT NULL
-
-
-
-
-
-
-
-
-
+	profileAvail BIT(1) NOT NULL,
 	profilePhone VARCHAR(32),
-
-	-- to make sure duplicate data cannot exist, create a unique index
 	UNIQUE(profileName),
 	UNIQUE(profileEmail),
-	-- this officiates the primary key for the entity
 	PRIMARY KEY(profileId)
 );
-CREATE TABLE eval(
-	-- this is for yet another primary key...
+CREATE TABLE eval (
 	evalId BINARY(16) NOT NULL,
-	-- this is for a foreign key
 	evalFromId BINARY(16) NOT NULL,
 	evalToId BINARY(16) NOT NULL,
 	evalContent VARCHAR(140) NOT NULL,
 	evalDate DATETIME(6) NOT NULL,
-
-	-- this creates the actual foreign key relation
 	FOREIGN KEY(evalFromId) REFERENCES profile(profileId),
 	FOREIGN KEY(evalToId) REFERENCES profile(profileId),
-	-- and finally create the primary key
 	PRIMARY KEY(evalId)
+);
+
+CREATE TABLE message (
+
+	messageId BINARY(16) NOT NULL,
+	messageFromId BINARY(16) NOT NULL,
+	messageToId BINARY(16) NOT NULL,
+	messageContent VARCHAR(280) NOT NULL,
+	messageDate DATETIME(6) NOT NULL,
+	FOREIGN KEY(messageFromId) REFERENCES profile(profileId),
+	FOREIGN KEY(messageToId) REFERENCES profile(profileId),
+	PRIMARY KEY(messageId)
+);
